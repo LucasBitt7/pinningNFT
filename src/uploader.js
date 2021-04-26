cc42cb1b00f73cab1fa784ecc92bec6dc5b21f49
thaymerapv <thaymerapv@gmail.com>
2021-04-25 20:21:10 -0400

    resolve fixs in console to get cid and other informations

--- src/uploader.js
+++ src/uploader.js
@@ -6,8 +6,15 @@ const getUploadParams = () => ({ url: 'https://nft.storage/api/upload',
 headers: { "Authorization": "Bearer API KEY"
  }})
 ;//"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5ASDASDAXCZg0NTY5MDEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYxODU5NzczODM5NCwibmFtZSI6ImtleTEifQ.uNeFoDDU_M8uzTNTVQ3uYnxejjVNldno5nFuxzoOWMk"
-const handleChangeStatus = ({ meta, file, cid }, status) => { console.log(status, meta, file, cid) }
-  
+const handleChangeStatus = ({ meta, file, xhr }, status) => {
+ if (xhr?.response){
+  const {value: {cid}} = JSON.parse(xhr.response)
+  console.log(`CID:${cid}`)
+  console.log('meta: ', meta)
+  console.log('file', file)
+  console.log(`other information: `, JSON.parse(xhr.response))
+}
+}
 const handleSubmit = (files, allFiles) => {
 console.log(files.map(f => f.meta))
 allFiles.forEach(f => f.remove())
@@ -20,7 +27,6 @@ onChangeStatus={handleChangeStatus}
 onSubmit={handleSubmit}
 accept='image/*, image/gif, audio/*, video/*, gif/*, .gif, .pdf, .mp3'
 inputContent={() => ( 'Drag Files')}
-
 />
 )
 }
